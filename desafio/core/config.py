f80f1dadcc7e9e9e2e8d69645229b76fe5edad3d
mts-lucas <lmateus1067@outlook.com>
2025-07-31 16:03:48 -0300

    feat: add core configs

---
+++ desafio/core/config.py
@@ -0,0 +1,37 @@
+from typing import ClassVar, List, Union
+
+from pydantic import Field, HttpUrl
+from pydantic_settings import BaseSettings
+
+
+
+class Settings(BaseSettings):
+
+
+    # Segurança
+    SECRET_KEY: str = Field(default="your-secret-key")
+    ALGORITHM: str = Field(default="HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=30)
+    ALLOW_ORIGINS: List[Union[HttpUrl, str]] =Field(default=["*"])
+    ALLOW_CREDENTIALS: bool =Field(default=True)
+    ALLOW_METHODS: List[str] =Field(default=["*"])
+    ALLOW_HEADERS: List[str] =Field(default=["*"])
+    CORS_CONFIG: ClassVar[dict] = {
+        "allow_origins": ALLOW_ORIGINS,
+        "allow_credentials": ALLOW_CREDENTIALS,
+        "allow_methods": ALLOW_METHODS,
+        "allow_headers": ALLOW_HEADERS,
+    }
+
+    # Informações do Projeto
+    PROJECT_NAME: str = Field(default="Desafio OP2B")
+    PROJECT_VERSION: str = Field(default="1.0.0")
+    DEBUG: bool = Field(default=False)
+
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()
