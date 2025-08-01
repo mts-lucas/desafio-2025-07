4806a745e0cbb71a717263e82ed21108adca385a
mts-lucas <lmateus1067@outlook.com>
2025-08-01 11:51:04 -0300

    chore: add width in schemas

--- desafio/schemas/text.py
+++ desafio/schemas/text.py
@@ -3,11 +3,14 @@ from pydantic import BaseModel
 
 class TextInput(BaseModel):
     text: str
+    width: int
 
 class FormattedText(BaseModel):
     original_text: str
     formatted_text: str
+    width: int
 
 class JustifiedText(BaseModel):
     original_text: str
     justified_text: str
+    width: int
